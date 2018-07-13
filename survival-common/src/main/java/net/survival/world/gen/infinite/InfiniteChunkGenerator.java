2512e68fe38cafa5c13fd1854333c1aaaa74ed8f
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2018-07-13 16:57:21 -0500

    Remove WorldDecorator

--- survival-common/src/main/java/net/survival/world/gen/infinite/InfiniteChunkGenerator.java
+++ survival-common/src/main/java/net/survival/world/gen/infinite/InfiniteChunkGenerator.java
@@ -2,22 +2,18 @@ package net.survival.world.gen.infinite;
 
 import net.survival.world.chunk.Chunk;
 import net.survival.world.chunk.ChunkGenerator;
-import net.survival.world.gen.infinite.decoration.WorldDecoratorStub;
 import net.survival.world.gen.infinite.terrain.WorldTerrain;
 
 public class InfiniteChunkGenerator implements ChunkGenerator
 {
     private final WorldTerrain worldTerrain;
-    private final WorldDecoratorStub worldDecoratorStub;
     
     public InfiniteChunkGenerator(long seed) {
         worldTerrain = new WorldTerrain(seed);
-        worldDecoratorStub = new WorldDecoratorStub();
     }
 
     @Override
     public Chunk generate(int cx, int cz) {
-        Chunk chunk = worldDecoratorStub.decorate(cx, cz, worldTerrain);
-        return chunk;
+        return worldTerrain.getChunkPrimer(cx, cz).toChunk();
     }
 }
--- survival-common/src/main/java/net/survival/world/gen/infinite/decoration/WorldDecorator.java
+++
@@ -1,9 +0,0 @@
-package net.survival.world.gen.infinite.decoration;
-
-import net.survival.world.chunk.Chunk;
-import net.survival.world.gen.infinite.terrain.WorldTerrain;
-
-public interface WorldDecorator
-{
-    public abstract Chunk decorate(int cx, int cz, WorldTerrain worldTerrain);
-}
