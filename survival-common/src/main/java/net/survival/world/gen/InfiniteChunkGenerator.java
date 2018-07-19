5824aaf2b2221c28f8b968532cd03f0e5e8305ab
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2018-07-18 20:41:29 -0500

    Move TerrainGenerator into InfiniteChunkGenerator
    Since net.survival.world.gen.terrain no longer has a purpose, terrain
    generation code has been moved to InfiniteChunkGenerator. Also,
    ChunkPrimer has been removed to avoid redundant copying.

--- survival-common/src/main/java/net/survival/world/gen/InfiniteChunkGenerator.java
+++ survival-common/src/main/java/net/survival/world/gen/InfiniteChunkGenerator.java
@@ -1,19 +1,163 @@
 package net.survival.world.gen;
 
+import net.survival.block.BlockType;
+import net.survival.util.DoubleMap2D;
+import net.survival.util.DoubleMap3D;
+import net.survival.util.ImprovedNoiseGenerator3D;
 import net.survival.world.chunk.Chunk;
 import net.survival.world.chunk.ChunkGenerator;
-import net.survival.world.gen.terrain.WorldTerrain;
+import net.survival.world.chunk.ChunkPos;
+import net.survival.world.gen.layer.GenLayer;
+import net.survival.world.gen.layer.GenLayerFactory;
 
 public class InfiniteChunkGenerator implements ChunkGenerator
 {
-    private final WorldTerrain worldTerrain;
-    
+    private static final int NBLOCK_YLENGTH = Chunk.YLENGTH / 32;
+    private static final int NBLOCK_ZLENGTH = Chunk.ZLENGTH / 4;
+    private static final int NBLOCK_XLENGTH = Chunk.XLENGTH / 4;
+    private static final int NMAP_YLENGTH = (Chunk.YLENGTH / NBLOCK_YLENGTH) + 1;
+    private static final int NMAP_ZLENGTH = (Chunk.ZLENGTH / NBLOCK_ZLENGTH) + 1;
+    private static final int NMAP_XLENGTH = (Chunk.XLENGTH / NBLOCK_XLENGTH) + 1;
+
+    private static final double MAIN_NOISE_XSCALE = 1.0 / 128.0;
+    private static final double MAIN_NOISE_YSCALE = 1.0 / 128.0;
+    private static final double MAIN_NOISE_ZSCALE = 1.0 / 128.0;
+    private static final int MAIN_NOISE_OCTAVE_COUNT = 6;
+
+    private static final int BIOME_TRANSITION_XLENGTH = NBLOCK_XLENGTH * 4;
+    private static final int BIOME_TRANSITION_ZLENGTH = NBLOCK_ZLENGTH * 4;
+    private static final int BIOME_TRANSITION_AREA = BIOME_TRANSITION_XLENGTH
+            * BIOME_TRANSITION_ZLENGTH;
+
+    private static final int OCEAN_LEVEL = 64;
+
+    private final ImprovedNoiseGenerator3D mainNoiseGenerator;
+    private final GenLayer biomeLayer;
+
+    private final DoubleMap3D densityMap;
+    private final DoubleMap2D minElevationMap;
+    private final DoubleMap2D elevationRangeMap;
+
     public InfiniteChunkGenerator(long seed) {
-        worldTerrain = new WorldTerrain(seed);
+        mainNoiseGenerator = new ImprovedNoiseGenerator3D(MAIN_NOISE_XSCALE, MAIN_NOISE_YSCALE,
+                MAIN_NOISE_ZSCALE, MAIN_NOISE_OCTAVE_COUNT, seed);
+
+        biomeLayer = GenLayerFactory.createBiomeLayer(Chunk.XLENGTH * 4, Chunk.ZLENGTH * 4, seed);
+
+        densityMap = new DoubleMap3D(NMAP_XLENGTH, NMAP_YLENGTH, NMAP_ZLENGTH);
+        minElevationMap = new DoubleMap2D(Chunk.XLENGTH, Chunk.ZLENGTH);
+        elevationRangeMap = new DoubleMap2D(Chunk.XLENGTH, Chunk.ZLENGTH);
     }
 
     @Override
     public Chunk generate(int cx, int cz) {
-        return worldTerrain.getChunkPrimer(cx, cz).toChunk();
+        int offsetX = cx * (NMAP_XLENGTH - 1);
+        int offsetZ = cz * (NMAP_ZLENGTH - 1);
+        int globalX = ChunkPos.toGlobalX(cx, 0);
+        int globalZ = ChunkPos.toGlobalZ(cz, 0);
+
+        mainNoiseGenerator.generate(densityMap, offsetX, 0.0, offsetZ);
+        biomeLayer.generate(globalX, globalZ);
+
+        Chunk chunk = new Chunk();
+        generateBase(chunk);
+        replaceBlocks(cx, cz, chunk);
+
+        return chunk;
+    }
+
+    private void replaceBlocks(int cx, int cz, Chunk chunk) {
+        for (int z = 0; z < Chunk.ZLENGTH; ++z) {
+            for (int x = 0; x < Chunk.XLENGTH; ++x) {
+                BiomeType biome = BiomeType.byID(biomeLayer.sampleNearest(x, z));
+                int state = 0;
+                int counter = 3;
+
+                for (int y = Chunk.YLENGTH - 1; y >= 0; --y) {
+                    if (chunk.getBlockID(x, y, z) != BlockType.TEMP_SOLID.id) {
+                        state = 0;
+                        counter = 3;
+                        continue;
+                    }
+
+                    switch (state) {
+                    case 0:
+                        chunk.setBlockID(x, y, z, biome.getTopBlockID());
+                        ++state;
+                        break;
+
+                    case 1:
+                        if (counter > 0) {
+                            chunk.setBlockID(x, y, z, BlockType.DIRT.id);
+                            --counter;
+
+                            if (counter == 0) {
+                                ++state;
+                                counter = 8;
+                            }
+                        }
+
+                        break;
+
+                    case 2:
+                        chunk.setBlockID(x, y, z, BlockType.STONE.id);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void generateBase(Chunk chunk) {
+        generateElevationMaps(minElevationMap, elevationRangeMap, chunk, biomeLayer);
+
+        for (int y = 0; y < Chunk.YLENGTH; ++y) {
+            double noiseMapY = (double) y / NBLOCK_YLENGTH;
+
+            for (int z = 0; z < Chunk.ZLENGTH; ++z) {
+                double noiseMapZ = (double) z / NBLOCK_ZLENGTH;
+
+                for (int x = 0; x < Chunk.XLENGTH; ++x) {
+                    double noiseMapX = (double) x / NBLOCK_XLENGTH;
+
+                    double density = densityMap.sampleLinear(noiseMapX, noiseMapY, noiseMapZ);
+
+                    double minElevation = minElevationMap.sampleNearest(x, z);
+                    double elevationRange = elevationRangeMap.sampleNearest(x, z);
+                    double threshold = (y - minElevation) / elevationRange;
+
+                    if (density >= threshold)
+                        chunk.setBlockID(x, y, z, BlockType.TEMP_SOLID.id);
+                    else if (y <= OCEAN_LEVEL)
+                        chunk.setBlockID(x, y, z, BlockType.WATER.id);
+                }
+            }
+        }
+    }
+
+    private void generateElevationMaps(DoubleMap2D minElevationMap, DoubleMap2D elevationRangeMap,
+            Chunk chunk, GenLayer genLayer)
+    {
+        for (int z = 0; z < Chunk.ZLENGTH; ++z) {
+            for (int x = 0; x < Chunk.XLENGTH; ++x) {
+                double avgMinElevation = 0.0;
+                double avgMaxElevation = 0.0;
+
+                for (int subZ = 0; subZ < BIOME_TRANSITION_ZLENGTH; ++subZ) {
+                    for (int subX = 0; subX < BIOME_TRANSITION_XLENGTH; ++subX) {
+                        BiomeType biome = BiomeType
+                                .byID(genLayer.sampleNearest(x + subX, z + subZ));
+                        avgMinElevation += biome.getMinElevation();
+                        avgMaxElevation += biome.getMaxElevation();
+                    }
+                }
+
+                avgMinElevation /= BIOME_TRANSITION_AREA;
+                avgMaxElevation /= BIOME_TRANSITION_AREA;
+
+                minElevationMap.setPoint(x, z, avgMinElevation);
+                elevationRangeMap.setPoint(x, z, avgMaxElevation);
+            }
+        }
     }
 }
