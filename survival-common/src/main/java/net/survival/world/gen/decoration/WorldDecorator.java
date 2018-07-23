eb04fa5e72df9b71f6db5592d15e0edd3069fc8a
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2018-07-23 15:16:47 -0500

    Add basic world decorator API
    World decorators add chunk-crossing features to the world. For example,
    the leaves of a tree are likely to cross one or more chunk borders.

---
+++ survival-common/src/main/java/net/survival/world/gen/decoration/WorldDecorator.java
@@ -0,0 +1,13 @@
+package net.survival.world.gen.decoration;
+
+import net.survival.world.World;
+import net.survival.world.chunk.Chunk;
+
+public interface WorldDecorator
+{
+    void decorate(int cx, int cz, Chunk chunk, World world);
+    
+    public static WorldDecorator createDefault() {
+        throw new RuntimeException("Not implemented yet.");
+    }
+}
