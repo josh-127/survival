ff110a73183905e3b56d0bf8b4e4e163f10d7c0a
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-02-15 02:57:16 -0600

    Add column decorators for remaining biomes

--- survival-common/src/main/java/net/survival/gen/DefaultColumnDecorator.java
+++ survival-common/src/main/java/net/survival/gen/DefaultColumnDecorator.java
@@ -4,14 +4,20 @@ import net.survival.gen.layer.GenLayer;
 
 class DefaultColumnDecorator extends ColumnDecorator
 {
-    private final OceanBiomeDecorator oceanBiomeDecorator = new OceanBiomeDecorator();
     private final DesertBiomeDecorator desertBiomeDecorator = new DesertBiomeDecorator();
+    private final ExtremeHillsDecorator extremeHillsDecorator = new ExtremeHillsDecorator();
+    private final ForestBiomeDecorator forestBiomeDecorator = new ForestBiomeDecorator();
+    private final GrasslandBiomeDecorator grasslandBiomeDecorator = new GrasslandBiomeDecorator();
+    private final OceanBiomeDecorator oceanBiomeDecorator = new OceanBiomeDecorator();
     private final CactusDecorator cactusDecorator = new CactusDecorator();
 
     @Override
     public void decorate(long columnPos, ColumnPrimer primer, GenLayer biomeMap) {
-        oceanBiomeDecorator.decorate(columnPos, primer, biomeMap);
         desertBiomeDecorator.decorate(columnPos, primer, biomeMap);
+        extremeHillsDecorator.decorate(columnPos, primer, biomeMap);
+        forestBiomeDecorator.decorate(columnPos, primer, biomeMap);
+        grasslandBiomeDecorator.decorate(columnPos, primer, biomeMap);
+        oceanBiomeDecorator.decorate(columnPos, primer, biomeMap);
         cactusDecorator.decorate(columnPos, primer, biomeMap);
     }
 }
---
+++ survival-common/src/main/java/net/survival/gen/ExtremeHillsDecorator.java
@@ -0,0 +1,62 @@
+package net.survival.gen;
+
+import net.survival.blocktype.BlockType;
+import net.survival.gen.layer.GenLayer;
+
+class ExtremeHillsDecorator extends ColumnDecorator
+{
+    private final int bedrockID = BlockType.BEDROCK.getFullID();
+    private final int stoneID = BlockType.STONE.getFullID();
+    private final int dirtID = BlockType.DIRT.getFullID();
+    private final int grassID = BlockType.GRASS_BLOCK.getFullID();
+    private final int tempSolidID = BlockType.TEMP_SOLID.getFullID();
+
+    @Override
+    public void decorate(long columnPos, ColumnPrimer primer, GenLayer biomeMap) {
+        for (var z = 0; z < ColumnPrimer.ZLENGTH; ++z) {
+            for (var x = 0; x < ColumnPrimer.XLENGTH; ++x) {
+                if (biomeMap.sampleNearest(x, z) == BiomeType.EXTREME_HILLS.ordinal())
+                    decorateStrip(x, z, primer);
+            }
+        }
+    }
+
+    private void decorateStrip(int x, int z, ColumnPrimer primer) {
+        primer.setBlockFullID(x, 0, z, bedrockID);
+
+        var state = 0;
+        var counter = 3;
+
+        for (var y = primer.getTopLevel(x, z); y >= 1; --y) {
+            if (primer.getBlockFullID(x, y, z) != tempSolidID) {
+                state = 0;
+                counter = 3;
+                continue;
+            }
+
+            switch (state) {
+            case 0:
+                primer.setBlockFullID(x, y, z, grassID);
+                ++state;
+                break;
+
+            case 1:
+                if (counter > 0) {
+                    primer.setBlockFullID(x, y, z, dirtID);
+                    --counter;
+
+                    if (counter == 0) {
+                        ++state;
+                        counter = 8;
+                    }
+                }
+
+                break;
+
+            case 2:
+                primer.setBlockFullID(x, y, z, stoneID);
+                break;
+            }
+        }
+    }
+}
