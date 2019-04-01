58216ba247e450b9d804297beefd379f517ae513
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-04-01 17:29:45 -0500

    Fix crash when placing blocks

--- survival-client/src/main/java/net/survival/client/graphics/blockrenderer/NorthStairsRenderer.java
+++ survival-client/src/main/java/net/survival/client/graphics/blockrenderer/NorthStairsRenderer.java
@@ -13,10 +13,12 @@ class NorthStairsRenderer extends BlockRenderer
     public void pushTopFaces(int x, int y, int z, int blockId, int adjacentBlockId,
             Builder builder)
     {
-        var u1 = topFaceTextures.getTexCoordU1(blockId);
-        var u2 = topFaceTextures.getTexCoordU2(blockId);
-        var v1 = topFaceTextures.getTexCoordV1(blockId);
-        var v2 = topFaceTextures.getTexCoordV2(blockId);
+        var typeId = BlockId.typeIdFromFullId(blockId);
+
+        var u1 = topFaceTextures.getTexCoordU1(typeId);
+        var u2 = topFaceTextures.getTexCoordU2(typeId);
+        var v1 = topFaceTextures.getTexCoordV1(typeId);
+        var v2 = topFaceTextures.getTexCoordV2(typeId);
 
         if (!blockToBlockingBottomTable[BlockId.typeIdFromFullId(adjacentBlockId)]) {
             builder.setTexCoord(u1, v1); builder.pushVertex(x,        y + 1.0f, z + 0.5f);
@@ -42,10 +44,12 @@ class NorthStairsRenderer extends BlockRenderer
         if (blockToBlockingTopTable[BlockId.typeIdFromFullId(adjacentBlockId)])
             return;
 
-        var u1 = bottomFaceTextures.getTexCoordU1(blockId);
-        var u2 = bottomFaceTextures.getTexCoordU2(blockId);
-        var v1 = bottomFaceTextures.getTexCoordV1(blockId);
-        var v2 = bottomFaceTextures.getTexCoordV2(blockId);
+        var typeId = BlockId.typeIdFromFullId(blockId);
+
+        var u1 = bottomFaceTextures.getTexCoordU1(typeId);
+        var u2 = bottomFaceTextures.getTexCoordU2(typeId);
+        var v1 = bottomFaceTextures.getTexCoordV1(typeId);
+        var v2 = bottomFaceTextures.getTexCoordV2(typeId);
         builder.setTexCoord(u1, v1); builder.pushVertex(x,        y, z       );
         builder.setTexCoord(u2, v1); builder.pushVertex(x + 1.0f, y, z       );
         builder.setTexCoord(u2, v2); builder.pushVertex(x + 1.0f, y, z + 1.0f);
@@ -61,10 +65,12 @@ class NorthStairsRenderer extends BlockRenderer
         if (blockToBlockingRightTable[BlockId.typeIdFromFullId(adjacentBlockId)])
             return;
 
-        var u1 = leftFaceTextures.getTexCoordU1(blockId);
-        var u2 = leftFaceTextures.getTexCoordU2(blockId);
-        var v1 = leftFaceTextures.getTexCoordV1(blockId);
-        var v2 = leftFaceTextures.getTexCoordV2(blockId);
+        var typeId = BlockId.typeIdFromFullId(blockId);
+
+        var u1 = leftFaceTextures.getTexCoordU1(typeId);
+        var u2 = leftFaceTextures.getTexCoordU2(typeId);
+        var v1 = leftFaceTextures.getTexCoordV1(typeId);
+        var v2 = leftFaceTextures.getTexCoordV2(typeId);
         builder.setTexCoord(u1, v1); builder.pushVertex(x, y,        z       );
         builder.setTexCoord(u2, v1); builder.pushVertex(x, y,        z + 0.5f);
         builder.setTexCoord(u2, v2); builder.pushVertex(x, y + 1.0f, z + 0.5f);
@@ -87,10 +93,12 @@ class NorthStairsRenderer extends BlockRenderer
         if (blockToBlockingLeftTable[BlockId.typeIdFromFullId(adjacentBlockId)])
             return;
 
-        var u1 = rightFaceTextures.getTexCoordU1(blockId);
-        var u2 = rightFaceTextures.getTexCoordU2(blockId);
-        var v1 = rightFaceTextures.getTexCoordV1(blockId);
-        var v2 = rightFaceTextures.getTexCoordV2(blockId);
+        var typeId = BlockId.typeIdFromFullId(blockId);
+
+        var u1 = rightFaceTextures.getTexCoordU1(typeId);
+        var u2 = rightFaceTextures.getTexCoordU2(typeId);
+        var v1 = rightFaceTextures.getTexCoordV1(typeId);
+        var v2 = rightFaceTextures.getTexCoordV2(typeId);
         builder.setTexCoord(u1, v1); builder.pushVertex(x + 1.0f, y,        z + 0.5f);
         builder.setTexCoord(u2, v1); builder.pushVertex(x + 1.0f, y,        z       );
         builder.setTexCoord(u2, v2); builder.pushVertex(x + 1.0f, y + 1.0f, z       );
@@ -110,10 +118,12 @@ class NorthStairsRenderer extends BlockRenderer
     public void pushFrontFaces(int x, int y, int z, int blockId, int adjacentBlockId,
             Builder builder)
     {
-        var u1 = frontFaceTextures.getTexCoordU1(blockId);
-        var u2 = frontFaceTextures.getTexCoordU2(blockId);
-        var v1 = frontFaceTextures.getTexCoordV1(blockId);
-        var v2 = frontFaceTextures.getTexCoordV2(blockId);
+        var typeId = BlockId.typeIdFromFullId(blockId);
+
+        var u1 = frontFaceTextures.getTexCoordU1(typeId);
+        var u2 = frontFaceTextures.getTexCoordU2(typeId);
+        var v1 = frontFaceTextures.getTexCoordV1(typeId);
+        var v2 = frontFaceTextures.getTexCoordV2(typeId);
 
         if (!blockToBlockingBackTable[BlockId.typeIdFromFullId(adjacentBlockId)]) {
             builder.setTexCoord(u1, v1); builder.pushVertex(x,        y,        z + 1.0f);
@@ -139,10 +149,12 @@ class NorthStairsRenderer extends BlockRenderer
         if (blockToBlockingFrontTable[BlockId.typeIdFromFullId(adjacentBlockId)])
             return;
 
-        var u1 = backFaceTextures.getTexCoordU1(blockId);
-        var u2 = backFaceTextures.getTexCoordU2(blockId);
-        var v1 = backFaceTextures.getTexCoordV1(blockId);
-        var v2 = backFaceTextures.getTexCoordV2(blockId);
+        var typeId = BlockId.typeIdFromFullId(blockId);
+
+        var u1 = backFaceTextures.getTexCoordU1(typeId);
+        var u2 = backFaceTextures.getTexCoordU2(typeId);
+        var v1 = backFaceTextures.getTexCoordV1(typeId);
+        var v2 = backFaceTextures.getTexCoordV2(typeId);
         builder.setTexCoord(u1, v1); builder.pushVertex(x + 1.0f, y,        z);
         builder.setTexCoord(u2, v1); builder.pushVertex(x,        y,        z);
         builder.setTexCoord(u2, v2); builder.pushVertex(x,        y + 1.0f, z);
